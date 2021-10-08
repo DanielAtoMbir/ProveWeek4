821f0300b50707aff140447f52aeb13f7f58584e
michelleguti17 <gut20014@byui.edu>
2021-10-08 12:35:44 -0500

    Hello from Michelle

--- src/Mbir/HelloFromDaniel.java
+++ src/Mbir/HelloFromDaniel.java
@@ -1,5 +1,8 @@
+
+
 public class HelloFromDaniel {
     public static void main (String args[]) {
         System.out.println("Hello from Daniel");
+        System.out.println("Hello Daniel, my name is Michelle");
     }
 }
